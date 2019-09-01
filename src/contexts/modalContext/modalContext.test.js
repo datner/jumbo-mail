8c4dbb418aeb7e46dc2679de5de13d2caf37ac8e
Datner <yuval.datner.alt@gmail.com>
2019-09-01 15:34:07 +0300

    write some tests for modal context

--- src/contexts/modalContext/modalContext.test.js
+++ src/contexts/modalContext/modalContext.test.js
@@ -1,26 +1,31 @@
+import React from "react";
+import { shallow } from "enzyme";
 import { createTestApi } from "utils/useApi";
 import modalFactory from "./modalFactory";
+import { modalInitState } from "./modalContext";
 
 /**
  * constant default value
  */
-const defaultValue = {
-    value: 1
-};
+const defaultValue = modalInitState;
+
+const testModal = createTestApi(modalFactory, defaultValue);
+afterEach(() => testModal.api.setModal(defaultValue));
 
 /**
  * initialize the testApi in a non-react enviroment
  * Remember, we are only checking if it works, not if its in React
  */
-const testModal = createTestApi(modalFactory, defaultValue);
 describe("modalContext", () => {
-  it("returns initial value", () => {
-    expect(testModal.api.getValue()).toEqual(defaultValue.value);
+  it("returns null when theres no Modal", () => {
+    expect(testModal.api.getModal()).toEqual(defaultValue.modal);
   });
 
-  it("can change value", () => {
-    testModal.api.setValue(2);  
-    expect(testModal.api.getValue()).toEqual(2);
-  });
+  it("can set a Modal directly", () => {
+    const NewModal = () => <div>I passed the test</div>;
 
+    testModal.api.setModal(NewModal);
+    const Modal = testModal.state.modal;
+    expect(shallow(<Modal />).text()).toEqual("I passed the test");
+  });
 });
