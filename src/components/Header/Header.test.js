f8dfc479ae60b2b66c4b3a64e2f1de96c6150a6d
Datner <yuval.datner.alt@gmail.com>
2019-09-01 15:33:35 +0300

    fix header test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Header from "./Header";
+import ModalProvider from "contexts/modalContext";
 
 it("renders without crashing", () => {
-  shallow(<Header />);
+  shallow(
+    <ModalProvider>
+      <Header />
+    </ModalProvider>
+  );
 });
